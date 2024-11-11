8a2aa3584433ce328ce853d69e0de00b954b7e7c
Jonathan Navas <jonathan.navas9@hotmail.com>
2024-11-11 18:28:53 -0500

    implement pre fetching

--- src/issues/components/IssueItem.tsx
+++ src/issues/components/IssueItem.tsx
@@ -1,5 +1,8 @@
+import { useQueryClient } from '@tanstack/react-query'
 import { FiCheckCircle, FiInfo, FiMessageSquare } from 'react-icons/fi'
 import { useNavigate } from 'react-router-dom'
+import { getIssue } from '../action/actions/get-issue'
+import { getIssueComments } from '../action/actions/get-issue-comments'
 import { GithubIssue, State } from '../interfaces/issue'
 
 interface Props {
@@ -9,8 +12,34 @@ interface Props {
 export const IssueItem = ({ issue }: Props) => {
   const navigate = useNavigate()
 
+  const queryClient = useQueryClient()
+
+  const handlePrefectData = () => {
+    queryClient.prefetchQuery({
+      queryKey: ['issues', issue.number],
+      queryFn: () => getIssue(issue.number),
+      staleTime: 1000 * 60,
+    })
+
+    queryClient.prefetchQuery({
+      queryKey: ['issues', issue.number, 'comments'],
+      queryFn: () => getIssueComments(issue.number),
+      staleTime: 1000 * 60,
+    })
+  }
+
+  const presetData = () => {
+    queryClient.setQueryData(['issues', issue.number], issue, {
+      updatedAt: Date.now() + 1000 * 60,
+    })
+  }
+
   return (
-    <div className="flex items-center px-2 py-3 mb-5 border rounded-md bg-slate-900 hover:bg-slate-800">
+    <div
+      className="flex items-center px-2 py-3 mb-5 border rounded-md bg-slate-900 hover:bg-slate-800"
+      // onMouseEnter={handlePrefectData}
+      onMouseEnter={presetData}
+    >
       {issue.state === State.Close ? (
         <FiInfo size={30} color="red" className="min-w-10" />
       ) : (
